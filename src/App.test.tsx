5de459c6970861a7a0040f8d3094edaa6a49b697
Vasu <vaasu.dg@gmail.com>
2023-08-02 14:48:38 -0700

    make fail test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/Demo/i)
+  const linkElement = screen.getByText(/Demo../i)
   expect(linkElement).toBeInTheDocument()
 })
