0d26708fbb4c268d5cea5af61c4f51a993efd42b
Vasu <vaasu.dg@gmail.com>
2023-08-03 03:19:04 -0700

    trying auto deploy1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       Demo
-      <p className='text-red-500'>Welcome!!!....</p>
+      <p className='text-red-500'>Welcome!!!....!!!!OWOWOWOW</p>
     </div>
   )
 }
